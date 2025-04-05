d78cebc5b5b845c5e1754009c8fe9c9ad74fca8e
Drogon4231 <hkantamneni2@wisc.edu>
2025-04-05 07:45:20 -0500

    feature extraction code changes

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -2,12 +2,19 @@
 import networkx as nx
 
 def extract_graph_features(circuit_file):
-    G = nx.Graph()
+    G = nx.DiGraph()  # Directed graph since tasks depend on others
+
     with open(circuit_file, 'r') as f:
-        for line in f:
-            src, dst = map(int, line.strip().split())
-            G.add_edge(src, dst)
-    
+        lines = f.readlines()
+        num_nodes = int(lines[0])  # First line is total number of nodes
+
+        for line in lines[1:]:
+            tokens = line.strip().split()
+            node_id = int(tokens[0])
+            deps = list(map(int, tokens[2:])) if len(tokens) > 2 else []
+            for d in deps:
+                G.add_edge(d, node_id)  # Add edge from dependency -> current node
+
     return {
         'num_nodes': G.number_of_nodes(),
         'num_edges': G.number_of_edges(),
